0a133399a1e60aca08cc3727a0e4fcab6774d049
Vaggelis Boutas <vaggbout@hotmail.com>
2020-11-22 17:20:00 +0200

    more error handling

--- event_handler.py
+++ event_handler.py
@@ -4,10 +4,15 @@ from services import data
 
 async def event_search_movie(message):
     # TODO: Check if message is PM or from channel somehow
+    search_terms = utils.remove_prefix(message.content, data.commands.get('movie_search'))
+    if search_terms.isspace() or not search_terms:
+        await message.channel.send(data.resposnes.get('no_args_movie'))
+        return
+        
     await message.channel.send(data.resposnes.get('standby'))
-    search_result = imdb_api.search_movie(utils.remove_prefix(message.content, '!search_movie'))
+    search_result = imdb_api.search_movie(search_terms)
     
-    if len(search_result) > 0:
+    if search_result:
         movie_info = imdb_api.get_movie_info(search_result[0])
         if movie_info:
             await message.channel.send(data.resposnes.get('found'))
--- services/data.py
+++ services/data.py
@@ -5,5 +5,6 @@ commands = {
 resposnes = {
     'standby': 'Ok, let me search this',
     'not_found': 'Sorry, unfortunately I didn\'t find something',
-    'found': 'I might have found something!'
+    'found': 'I might have found something!',
+    'no_args_movie': 'You must give something to search for!'
 }
